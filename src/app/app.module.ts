22676b057cde33b0821a76aee3cd8553faa6dbc4
ext.ddrozd <d.drozd@andersenlab.com>
2019-06-12 09:18:30 +0300

    page1-07: body init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderModule} from './header/header.module';
 import {AsideModule} from './aside/aside.module';
+import {BodyModule} from './body/body.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         HeaderModule,
-        AsideModule
+        AsideModule,
+        BodyModule
     ],
     declarations: [
         AppComponent
