eb5f763e7040cf2b6019e2f7b2d940614aca590b
ext.ddrozd <d.drozd@andersenlab.com>
2019-06-19 23:33:42 +0300

    page-3: результат + список неправильных ответов + скролл к неотвеченным

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as q01 from './tests/quiz01.json';
 import * as q02 from './tests/quiz02.json';
 
@@ -9,6 +9,9 @@ import * as q02 from './tests/quiz02.json';
 })
 export class BodyComponent {
     selectedQuiz;
+    result;
+    firstCheck = true;
+    successTest = false;
 
     quizList = [
         q01
@@ -16,5 +19,41 @@ export class BodyComponent {
 
     selectQuiz(list) {
         this.selectedQuiz = list;
+        this.firstCheck = true;
+        this.successTest = false;
+    }
+
+    checkResult() {
+        this.firstCheck = false;
+        this.result = 0;
+        
+        let arr = this.selectedQuiz.default.list;
+        for (let counter = 0; counter < arr.length; counter++) {
+            if(arr[counter].selected === null) {
+                this.scrollToEmptyQuestion('id' + (counter + 1));
+                break;
+            }
+
+            if(arr[counter].selected === arr[counter].rightAnswer) {
+                this.result += 1;
+            }
+
+            if((counter + 1) === arr.length) {
+                this.successTest = true;
+            }
+        }
+    }
+
+    scrollToEmptyQuestion(id) {
+        let el = document.getElementById(id);
+        el.scrollIntoView({block: "center", behavior: "smooth"});
+    }
+
+    checkQuestion(item) {
+        return item.selected === item.rightAnswer ? true : false;
+    }
+
+    highlightError(item, index) {
+        return item.rightAnswer === (index + 1) ? true : false;
     }
 }
